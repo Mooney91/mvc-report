9d99ff28d3717cd548b9149c06bc2a6055973abc
Zachary Mooney <zamo22@student.bth.se>
2023-04-29 10:14:01 +0200

    Add documentation to Class

--- src/Game/Game.php
+++ src/Game/Game.php
@@ -7,6 +7,10 @@ use App\Game\Player;
 use App\Game\Banker;
 use App\Card\Card;
 
+/**
+ * This is the main functionality for the game "Twenty-One"
+ * @author Zachary Mooney
+ */
 class Game
 {
     protected object $deck;
@@ -14,12 +18,32 @@ class Game
      * @var array<object> An array of cards
      */
     protected array $players = [];
+    /**
+     * This is the current player in play.
+     * @var object
+     */
     protected object $currentPlayer;
+    /**
+     * This is the Banker in the game.
+     * @var object
+     */
     protected object $banker;
+    /**
+     * This is the human in the game. Not very useful - may become deprecated.
+     * @var object
+     */
     protected object $human;
+    /**
+     * This is the winner of game.
+     * @var object
+     */
     protected object $winner;
 
-    public function __construct(int$num)
+    /**
+     * Create a Game object
+     * @param int   $num    The number of players
+     */
+    public function __construct(int$num = 1)
     {
         for ($i = 1; $i <= $num; $i++) {
             $this->setPlayer(strval($i));
@@ -32,6 +56,11 @@ class Game
         $this->currentPlayer = $this->players[0];
     }
 
+    /**
+     * Add a new player to the players attribute
+     * @param string   $identifier    The name of the player (this is usually numeric)
+     * @return object  $player        Return the player
+     */
     public function setPlayer(string $identifier): object
     {
         $player = new Player($identifier, $this);
@@ -40,6 +69,10 @@ class Game
         return $player;
     }
 
+    /**
+     * Add the Banker to the players attribute
+     * @return object   $banker The banker
+     */    
     public function setBanker(): object
     {
         $banker = new Banker("Banker", $this);
@@ -50,6 +83,7 @@ class Game
     }
 
     /**
+    * Return an array of all the Players 
     * @return object[]
     */
     public function getPlayers(): array
@@ -58,6 +92,7 @@ class Game
     }
 
     /**
+    * Return an associative array of the players' points
     * @return array<string, int>
     */
     public function getPlayersPoints(): array
@@ -71,16 +106,27 @@ class Game
         return $playersPoints;
     }
 
+    /**
+    * Get the Banker of the game
+    * @return object   The banker
+    */ 
     public function getBanker(): object
     {
         return $this->banker;
     }
 
+    /**
+    * Get the Human of the game
+    * @return object   The human (which in this version, is the Player)
+    */ 
     public function getHuman(): object
     {
         return $this->human;
     }
 
+    /**
+    * Change the current player to the next player in the Players array
+    */ 
     public function nextPlayer(): void
     {
         $current = $this->getCurrentPlayer();
@@ -91,16 +137,29 @@ class Game
         }
     }
 
+    /**
+    * Get the Current Player of the game
+    * @return object   The current player attribute
+    */ 
     public function getCurrentPlayer(): object
     {
         return $this->currentPlayer;
     }
 
+    /**
+    * Get the Deck of Cards that is used in the game
+    * @return object   The desk of cards
+    */ 
     public function getDeck(): object
     {
         return $this->deck;
     }
 
+    /**
+    * Return the points that a particular card has.
+    * @param object       $card    A Card object
+    * @return int         The number of points
+    */ 
     public function returnPoints(object $card): int
     {
         $face = $card->getFace();
@@ -122,6 +181,11 @@ class Game
         return $values[$face];
     }
 
+    /**
+    * Set the player who won as the winner and returns their name
+    * @param object       $player    The player who won
+    * @return string      The winner/player's name as a string
+    */ 
     public function victory(object $player): string
     {
         $this->winner = $player;
@@ -129,6 +193,10 @@ class Game
         return strval($player->getIdentifier());
     }
 
+    /**
+    * Return the winner of the game
+    * @return object     The winner
+    */ 
     public function getWinner(): object
     {
         return $this->winner;
