fd630783aa5c7edd2bbbea229146bc2a9a1ab738
Zachary Mooney <zamo22@student.bth.se>
2023-05-03 04:27:24 +0200

    Add Library table Report

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Library;
+use App\Repository\LibraryRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class LibraryController extends AbstractController
+{
+    #[Route('/library', name: 'app_library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    #[Route('/library/create', name: 'library_create')]
+    public function createBook(
+        ManagerRegistry $doctrine
+    ): Response {
+        $entityManager = $doctrine->getManager();
+    
+        $book = new Library();
+        $book->setTitle('temp_title_' . rand(1, 9));
+        $book->setAuthor('temp_author_' . rand(1, 9));
+        $book->setIsbn('temp_isbn_' . rand(1, 9));
+        $book->setPicture('temp_picture_' . rand(1, 9));
+    
+        // tell Doctrine you want to (eventually) save the Library
+        // (no queries yet)
+        $entityManager->persist($book);
+    
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+    
+        return new Response('Saved new book with id '.$book->getId());
+    }
+
+    #[Route('/library/show', name: 'library_show_all')]
+    public function showAllLibrary(
+        LibraryRepository $libraryRepository
+    ): Response {
+        $books= $libraryRepository
+            ->findAll();
+
+        $response = $this->json($books);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route('/library/show/{id}', name: 'library_by_id')]
+    public function showLibraryById(
+        LibraryRepository $libraryRepository,
+        int $id
+    ): Response {
+        $book = $libraryRepository
+            ->find($id);
+
+        return $this->json($book);
+    }
+
+    #[Route('/library/delete/{id}', name: 'library_delete_by_id')]
+    public function deleteLibraryById(
+        LibraryRepository $libraryRepository,
+        int $id
+    ): Response {
+        $book = $libraryRepository->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $libraryRepository->remove($book, true);
+
+        return $this->redirectToRoute('library_show_all');
+    }
+
+    #[Route('/library/update/{id}/{picture}', name: 'library_update')]
+    public function updateLibrary(
+        LibraryRepository $libraryRepository,
+        int $id,
+        string $picture
+    ): Response {
+        $book = $libraryRepository->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $book->setPicture($picture);
+        $libraryRepository->save($book, true);
+
+        return $this->redirectToRoute('library_show_all');
+    }
+}
