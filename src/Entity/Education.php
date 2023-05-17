ddaf6233553e68b538df5f8cb87f6ecd00bf7ac0
Zachary Mooney <zamo22@student.bth.se>
2023-05-17 03:43:02 +0200

    Create routes for Project

---
+++ src/Entity/Education.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EducationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EducationRepository::class)]
+class Education
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $gender = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $education_level = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $year = null;
+
+    #[ORM\Column]
+    private ?float $percentage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getEducationLevel(): ?string
+    {
+        return $this->education_level;
+    }
+
+    public function setEducationLevel(string $education_level): self
+    {
+        $this->education_level = $education_level;
+
+        return $this;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function setYear(string $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getPercentage(): ?float
+    {
+        return $this->percentage;
+    }
+
+    public function setPercentage(float $percentage): self
+    {
+        $this->percentage = $percentage;
+
+        return $this;
+    }
+}
